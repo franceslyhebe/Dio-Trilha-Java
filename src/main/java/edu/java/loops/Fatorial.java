26879fd8b06d91536e34dccafee102914bcbd582
Francesly Hebe <f.francesly@gmail.com>
2023-02-12 18:38:09 -0300

    Continuando exercicios

--- src/main/java/edu/java/loops/Fatorial.java
+++ src/main/java/edu/java/loops/Fatorial.java
@@ -1,4 +1,31 @@
 package edu.java.loops;
 
+import java.util.Scanner;
+
+/*
+Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
+Ex.: 5!= 120 (5 X 4 X 3 X 2 X 1)
+*/
 public class Fatorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number;
+
+        System.out.println("==========================" +
+                "\nFATORIAL" +
+                "\n==========================");
+
+        System.out.println("Digite o número desejado: ");
+        number = scanner.nextInt();
+
+        int multiplicacao = 1;
+
+        for(int i = number ; i >= 1 ; i --) {
+            multiplicacao = multiplicacao * i;
+        }
+
+        System.out.println("O fatorial de " + number + " é: "+ multiplicacao);
+
+    }
 }
--- src/main/java/edu/java/loops/ParEImpar.java
+++ src/main/java/edu/java/loops/ParEImpar.java
@@ -1,4 +1,35 @@
 package edu.java.loops;
 
+import java.util.Scanner;
+
+/*
+Faça um programa que peça N números inteiros,
+calcule e mostre a quantidade de números pares
+e a quantidade de números impares.
+*/
 public class ParEImpar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number, numbers, countOdd = 0, countEven = 0;
+
+        System.out.println("Digite a quantidade de números que será analisados: ");
+        number = scanner.nextInt();
+
+        int count = 0;
+        do {
+            System.out.println("Número: ");
+            numbers = scanner.nextInt();
+
+            if (numbers % 2 == 0 ) countEven++;
+            else countOdd++;
+
+            count++;
+        } while(count < number);
+
+        System.out.println("Quantidade Par: " + countOdd);
+        System.out.println("Quantidade Ímpar: " + countEven);
+
+
+    }
 }
