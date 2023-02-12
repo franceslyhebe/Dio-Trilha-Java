aec2344be152d56af39d8dbd6feb0d6cce3507e0
Francesly Hebe <f.francesly@gmail.com>
2023-02-12 15:53:17 -0300

    Exercicio JavaDoc

---
+++ src/main/java/edu/java/Calculadora/Calculadora.java
@@ -0,0 +1,24 @@
+package edu.java.Calculadora;
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre numeros inteiros
+ * <p>
+ *     <b>Note:</b> Leia atentamente a documentação desta classe
+ *     para desfrutar dos recursoso oferecidos pelo autor.
+ * </p>
+ * @author Fran
+ * @version 1.0
+ * @since 2023
+ *
+ * */
+public class Calculadora {
+    /**
+     * Este metodo é utilizado para somar dois numeros inteiros
+     * @param numeroUm é o primeiro parametro do metodo
+     * @param numeroDois é o segundo paramentro do metodo
+     * @return o resultado deste metodo é a soma dos dois numeros
+     * */
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+}
